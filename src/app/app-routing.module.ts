1ee5f142baab3966b63b5cea1ca0ebe50911152c
Eric Speelman <eric.t.speelman@gmail.com>
2019-08-31 11:09:45 -0400

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/account', pathMatch: 'full'},
   {path: 'portable', loadChildren: () => import('./portable/portable.module').then(m => m.PortableModule)},
-  {path: '', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
   { path: 's', loadChildren: () => import('./stand/stand.module').then(m => m.StandModule)}
 ];
 
