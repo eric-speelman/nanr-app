4700b5b5124136d7a6d970d3e4aa1ea93c0a441f
Eric Speelman <eric.t.speelman@gmail.com>
2019-08-31 11:01:35 -0400

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'portable', loadChildren: () => import('./portable/portable.module').then(m => m.PortableModule)},
-  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
+  {path: '', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
   { path: 's', loadChildren: () => import('./stand/stand.module').then(m => m.StandModule)}
 ];
 
