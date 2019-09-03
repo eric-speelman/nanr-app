fcc8fd8087e9ef3b97d41e3f667acad141db6e27
Eric Speelman <eric.t.speelman@gmail.com>
2019-09-03 17:07:28 -0400

    fixed issue with nanr stand test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://nanr-test.azurewebsites.net/api/',
   nanrStandUrl: 'https://nanr-test.azureedge.net/s/',
-  buttonUrl: 'https://nanr-test.azureedge.net/assets/tags/button.html'
+  buttonUrl: 'https://nanr-test-app.azureedge.net/assets/tags/button.html'
 };
