34d4066400afc557cd9db7802ac7799b45d76e15
Eric Speelman <eric.t.speelman@gmail.com>
2019-09-25 06:49:20 -0400

    Fix issues with build env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nanr.io/api/',
+  apiUrl: 'https://nanr-prod.azurewebsites.net/api/',
   nanrStandUrl: 'https://nanr.io/s/',
   buttonUrl: 'https://nanr.io/assets/tags/button.html'
 };
