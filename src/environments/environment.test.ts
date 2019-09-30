2b78aca77ec059aabb1e1a8c9650661fa19095d2
Eric Speelman <espeelman@rxsense.com>
2019-09-29 20:05:20 -0400

    c

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://nanr-test.azurewebsites.net/api/',
   nanrStandUrl: 'https://test.nanr.io/s/',
-  buttonUrl: 'https://app.test.nanr.io/assets/tags/button.html'
+  buttonUrl: 'https://app.test.nanr.io/assets/tags/button.html',
+  squareAppId: 'sandbox-sq0idb-1JUoOnGGfhxgP3ukD_d5uA'
 };
